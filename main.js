65be532298ac7fb8ab0aae676a0729b3bcdccdcc
Mr_Crafter <80324603+MrCrafterGlitched@users.noreply.github.com>
2021-10-01 15:45:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ leftWrist_x=""
 leftWrist_y=""
 rightWrist_x=""
 rightWrist_y=""
+leftWrist_score=""
 function preload() {
 Song=loadSound("music.mp3")    
 }
 function setup() {
-    canvas=createCanvas(300,300)
+    canvas=createCanvas(600,500)
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
@@ -17,13 +18,10 @@ function setup() {
 function model_loaded() {
     console.log("model is loaded!")
 }
-function draw() {
-    image(video,0,0,300,300)
-}
 function play() {
     Song.play()
-    Song.setVolume(0.5)
-    Song.rate(2.5)
+    Song.setVolume(1)
+    Song.rate(1)
 }
 function pause() {
     Song.pause()
@@ -39,5 +37,20 @@ function getPoses(Results) {
         rightWrist_x=Results[0].pose.rightWrist.x
         rightWrist_y=Results[0].pose.rightWrist.y
         console.log(leftWrist_x,leftWrist_y,rightWrist_x,rightWrist_y)
+         leftWrist_score=Results[0].pose.keypoints[9].score
+         console.log(leftWrist_score)
+    }
+}
+function draw() {
+    image(video,0,0,600,500)
+    fill("red")
+    stroke("orange")
+    if (leftWrist_score>0.2) {
+        circle(leftWrist_x,leftWrist_y,20)
+    leftWrist_y_number=Number(leftWrist_y)
+    removeDecimals=floor(leftWrist_y_number)
+    volume=removeDecimals/500
+    Song.setVolume(volume)
+    document.getElementById("volume").innerHTML="Volume-" + volume
     }
 }
